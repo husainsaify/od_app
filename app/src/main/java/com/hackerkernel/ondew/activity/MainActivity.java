9c97834dc5030b8e1a76cf253b845f6b12291b04
husainsaify <hackerkernel15@gmail.com>
2016-04-18 17:05:01 +0530

    completed OtpVerificationActivity

---
+++ app/src/main/java/com/hackerkernel/ondew/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.hackerkernel.ondew.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.hackerkernel.ondew.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.number_edittext) EditText mNumberView;
+    @Bind(R.id.login_button) Button mLoginView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+}
